e19eabd5bc15d9bc04fb5a2dda3c2432dd32fbbf
ShivaniMhatre <shivanikatkar97@gmail.com>
2024-10-15 17:34:26 +0530

    user model

---
+++ api/models/user.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        default: "https://img.freepik.com/premium-photo/stylish-man-flat-vector-profile-picture-ai-generated_606187-310.jpg"
+    },
+}, { timestamps: true })
+
+const User = mongoose.model('User', userSchema)
+
+export default User
