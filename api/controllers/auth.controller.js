8cb12a59f2818d14fae81790182b9f7ac816e769
ShivaniMhatre <shivanikatkar97@gmail.com>
2024-10-16 11:18:57 +0530

    create signup api route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,23 @@
+import User from "../models/user.model.js";
+import bcryptjs from 'bcryptjs'
+
+export const signup = async (req, res) => {
+   const { username, email, password } = req.body;
+   const hashPass = bcryptjs.hashSync(password, 10);
+   const newUser = new User({
+      username,
+      email,
+      password: hashPass
+   });
+   try {
+      await newUser.save();
+      res.status(200)
+         .json({
+            message: "User Created Successfully"
+         })
+   } catch (error) {
+      res.status(500).json({
+         message: error.message
+      })
+   }
+}
--- api/index.js
+++ api/index.js
@@ -2,17 +2,21 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
+import authRouter from './routes/auth.route.js'
 
 const app = express();
+app.use(express.json());
 
-app.listen(4000,()=>{
-    console.log("Server running on Port 4000!!");
+app.listen(3000, () => {
+    console.log("Server running on Port 3000!!");
 })
 
 mongoose.connect(process.env.MONGO)
-.then(()=>{
-    console.log("Connected to MongoDB!!");
-})
-.catch((err)=>{
-    console.log(err);
-})
+    .then(() => {
+        console.log("Connected to MongoDB!!");
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+
+app.use('/api/auth', authRouter)
