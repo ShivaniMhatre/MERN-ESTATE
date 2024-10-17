92081d79ed86067b3fa8a67d3781d7578f4eafcd
ShivaniMhatre <shivanikatkar97@gmail.com>
2024-10-17 12:22:26 +0530

    complete uer update api

--- api/index.js
+++ api/index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-dotenv.config();
+import cookieParser from 'cookie-parser';
 import authRouter from './routes/auth.route.js'
+import userRouter from './routes/user.route.js'
+dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000, () => {
     console.log("Server running on Port 3000!!");
@@ -20,6 +23,7 @@ mongoose.connect(process.env.MONGO)
     })
 
 app.use('/api/auth', authRouter)
+app.use('/api/user', userRouter)
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
