9eb53825650de05c631e6de8dc08fa4dc8831bb3
ShivaniMhatre <shivanikatkar97@gmail.com>
2024-11-05 11:14:47 +0530

    create search api route

--- api/controllers/list.controller.js
+++ api/controllers/list.controller.js
@@ -1,10 +1,11 @@
+import Listing from "../models/list.model.js"
 import List from "../models/list.model.js"
 import { errorHandler } from "../utils/error.js"
 
 export const createListing = async (req, res, next) => {
     try {
         const listing = await List.create(req.body)
-        return res.status(201).json( listing )
+        return res.status(201).json(listing)
     } catch (err) {
         next(err)
     }
@@ -53,6 +54,51 @@ export const getListing = async (req, res, next) => {
             return next(errorHandler(404, "Listing Not Found"))
         }
         res.status(200).json(listing)
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const getListings = async (req, res, next) => {
+    try {
+        const limit = parseInt(req.query.limit) || 9;
+        const startIndex = parseInt(req.query.startIndex) || 0;
+        let offer = req.query.offer;
+
+        if (offer === undefined || offer === 'false') {
+            offer = { $in: [false, true] }
+        }
+
+        let furnished = req.query.furnished;
+        if (furnished === undefined || furnished === 'false') {
+            furnished = { $in: [false, true] }
+        }
+
+        let parking = req.query.parking;
+        if (parking === undefined || parking === 'false') {
+            parking = { $in: [false, true] }
+        }
+
+        let type = req.query.type;
+        if (type === undefined || type === 'all') {
+            type = { $in: ['rent', 'sale'] }
+        }
+
+        const searchTerm = req.query.searchTerm || '';
+        const sort = req.query.sort || 'createdAt';
+        const order = req.query.order || 'desc';
+
+        const listings = await Listing.find({
+            name: { $regex: searchTerm, $options: 'i' },
+            offer,
+            furnished,
+            parking,
+            type
+        }).sort(
+            { [sort]: order }
+        ).limit(limit).skip(startIndex);
+
+        return res.status(200).json(listings); 
     } catch (error) {
         next(error)
     }
